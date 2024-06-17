9e3f630f5d91531d4b16d149dbab93ee226d9095
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-17 10:51:56 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const keys = Object.keys(data);
+    if (keys.length === 0) return;
+    const list = document.createElement("ul");
+    for (const key of keys){
+        const item = document.createElement("li");
+        item.insertAdjacentText("beforeend", key);
+        createTree(item, data[key]);
+        list.insertAdjacentElement("beforeend", item);
+    }
+    element.insertAdjacentElement("beforeend", list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
